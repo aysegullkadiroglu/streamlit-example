2dc2a7f9915d1ca19e6eb457825c0c99c9f159c7
Aysegul Kadiroglu <46954286+aysegullkadiroglu@users.noreply.github.com>
2023-11-07 17:07:44 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,50 +7,42 @@ Original file is located at
     https://colab.research.google.com/drive/1wCfitPn6Az0rtP38HpCeCLpxPQVuBRyR
 """
 
-# @title Setup code
-# pip install streamlit
-
-# Commented out IPython magic to ensure Python compatibility.
-# %%writefile app.py
-
-# pip install pyngrok
-
-# from google.colab import drive
-# drive.mount('/content/drive')
-
-import numpy as np
 import pickle
 import streamlit as st
-import os
 
 # yukarıdakileri fonkisyona alalım
 def recommend_movie(movie):
   index = netflix_data_v2[netflix_data_v2['TITLE']==movie].index[0]
   # benzerlik değerlerine göre sıralayıp uzaklık değerine atıyoruz
-  distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector:vector[1])
+  distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x:x[1])
   recommended_movies_name = []
-  for index, _ in distance[:10]:
-     index = netflix_data_v2.iloc[i[0]]['index']
-     recommended_movies_name.append(netflix_data_v2['TITLE'])
+  # Use the recommendations to get movie indices
+  recommended_movie_indices = [i[0] for i in distance[:10]]
+  # Extract and display the recommended movie titles
+  recommended_movie_titles = netflix_data_v2.iloc[recommended_movie_indices]['TITLE']
+  st.write("Recommended Movies:")
+  st.write(recommended_movie_titles)
+
   return recommended_movies_name
 
-st.header("Netflix Movie Recommendation System")
-# Get the full file path to 'netflix_titles.pkl'
-file_path = os.path.abspath('netflix_titles.pkl')
 
-# Load the file using the full path
-netflix_data_v2 = np.load(file_path, allow_pickle=True)
+html_temp = """
+<div style="background:#025246 ;padding:10px">
+<h2 style="color:white;text-align:center;"> Netflix Movie Recommendation </h2>
+</div>
+    """
+st.markdown(html_temp, unsafe_allow_html = True)
+st.markdown("<br>", unsafe_allow_html=True)
+netflix_data_v2 = pickle.load(open('artifacts/netflix_titles.pkl','rb'))
+similarity = pickle.load(open('artifacts/recommend_movie.pkl','rb'))
+
 
 movie_list = netflix_data_v2['TITLE'].values
 select_movie = st.selectbox(
     "Please type or select a movie for recommendation",
     movie_list
 )
-
-if st.button("Show recommendation"):
-    recommended_movies_name = recommend_movie(select_movie)
-
-    # Display the recommended movie titles
-    st.write("Recommended Movies:")
-    for movie_name in recommended_movies_name:
-        st.write(movie_name)
+st.markdown("<br>", unsafe_allow_html=True)
+if st.button('Show Recommendation'):
+    recommended_movie_names = recommend_movie(select_movie)
+   
